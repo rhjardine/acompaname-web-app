08585a1cb7c717adbbaa8ea09b0d8b0837f69daf
Perestrelo95 <perestrelo95@gmail.com>
2022-08-07 04:31:24 +0000

    mejora en los botones de las card

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -38,110 +38,115 @@ export const Home = () => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <div className="container w-100%">
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1567067974934-75a3e4534c14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-              alt="PRUEBA 1"
-            />
-            <Carousel.Caption>
-              <h3>Aqui estamos para ti</h3>
-              <p>
-                "Queremos acompañarte en tu proceso de salud con alternativas
-                que mejoren tu calidad de vida"
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1610019354509-d7501ad50598?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1458&q=80"
-              alt="PRUEBA 2"
-            />
+      <div className="container w-75%">
+        <div className="container w-75%">
+          <div className="container w-75%">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1567067974934-75a3e4534c14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                  alt="PRUEBA 1"
+                />
+                <Carousel.Caption>
+                  <h3>Aqui estamos para ti</h3>
+                  <p>
+                    "Queremos acompañarte en tu proceso de salud con
+                    alternativas que mejoren tu calidad de vida"
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1610019354509-d7501ad50598?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1458&q=80"
+                  alt="PRUEBA 2"
+                />
 
-            <Carousel.Caption>
-              <h3>Conciencia de Salud</h3>
-              <p>
-                "Sabemos que podemos darte herramientas para mejorar tu calidad
-                de vida"
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1508963493744-76fce69379c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-              alt="PRUEBA 3"
-            />
+                <Carousel.Caption>
+                  <h3>Conciencia de Salud</h3>
+                  <p>
+                    "Sabemos que podemos darte herramientas para mejorar tu
+                    calidad de vida"
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1508963493744-76fce69379c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                  alt="PRUEBA 3"
+                />
 
-            <Carousel.Caption>
-              <h3>Estilo de Vida Saludable</h3>
-              <p>
-                Sabias que las enfermedad de Alzheimer viene condicionada por un
-                20% de componente genetico y mas de 70% de aspectos
-                epigéneticos.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1459267809427-0b40207b2cb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="PRUEBA 3"
-            />
+                <Carousel.Caption>
+                  <h3>Estilo de Vida Saludable</h3>
+                  <p>
+                    Sabias que las enfermedad de Alzheimer viene condicionada
+                    por un 20% de componente genetico y mas de 70% de aspectos
+                    epigéneticos.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1459267809427-0b40207b2cb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  alt="PRUEBA 3"
+                />
 
-            <Carousel.Caption>
-              <h3>Estilo de Vida Saludable</h3>
-              <p>
-                Sabias que las enfermedad de Alzheimer viene condicionada por un
-                20% de componente genetico y mas de 70% de aspectos
-                epigéneticos.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1572567981653-ce74f7356946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
-              alt="PRUEBA 3"
-            />
+                <Carousel.Caption>
+                  <h3>Estilo de Vida Saludable</h3>
+                  <p>
+                    Sabias que las enfermedad de Alzheimer viene condicionada
+                    por un 20% de componente genetico y mas de 70% de aspectos
+                    epigéneticos.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1572567981653-ce74f7356946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+                  alt="PRUEBA 3"
+                />
 
-            <Carousel.Caption>
-              <h3>Estilo de Vida Saludable</h3>
-              <p>
-                Sabias que las enfermedad de Alzheimer viene condicionada por un
-                20% de componente genetico y mas de 70% de aspectos
-                epigéneticos.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1513159446162-54eb8bdaa79b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="PRUEBA 3"
-            />
+                <Carousel.Caption>
+                  <h3>Estilo de Vida Saludable</h3>
+                  <p>
+                    Sabias que las enfermedad de Alzheimer viene condicionada
+                    por un 20% de componente genetico y mas de 70% de aspectos
+                    epigéneticos.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1513159446162-54eb8bdaa79b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  alt="PRUEBA 3"
+                />
 
-            <Carousel.Caption>
-              <h3>Estilo de Vida Saludable</h3>
-              <p>
-                Sabias que las enfermedad de Alzheimer viene condicionada por un
-                20% de componente genetico y mas de 70% de aspectos
-                epigéneticos.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+                <Carousel.Caption>
+                  <h3>Estilo de Vida Saludable</h3>
+                  <p>
+                    Sabias que las enfermedad de Alzheimer viene condicionada
+                    por un 20% de componente genetico y mas de 70% de aspectos
+                    epigéneticos.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </div>
       </div>
       <Container>
-        <Row xs={1} md={5} className="g-4">
+        <Row xs={1} md={5} className="g-4" justify-content-md-center>
           <Card
             style={{
               width: "18rem",
               padding: "20px",
               margin: "40px 10px 40px 10px",
+              alignItems: "center",
             }}
             className="bg-success"
           >
@@ -157,9 +162,11 @@ export const Home = () => {
                 Conoce tu edad biológica y el riesgo precoz de padecer la
                 enfermedad de Alzheimer.
               </Card.Text>
-              <Button variant="light" size="lg">
-                <h3>Accede</h3>
-              </Button>
+              <div className="d-grid gap-2">
+                <Button variant="light" size="lg">
+                  <h3>Accede</h3>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
           <Card
@@ -182,13 +189,11 @@ export const Home = () => {
                 Explore los nuevos avances para tratar la enfermedad de
                 Alzheimer
               </Card.Text>
-              <Button
-                variant="light"
-                size="lg"
-                style={{ alignItems: "center" }}
-              >
-                <h3>Accede</h3>
-              </Button>
+              <div className="d-grid gap-2">
+                <Button variant="light" size="lg">
+                  <h3>Accede</h3>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
           <Card
@@ -211,9 +216,11 @@ export const Home = () => {
                 Conoce la ubicación de tu ser querido usando geolocalización
                 geográfica.
               </Card.Text>
-              <Button variant="light" size="lg">
-                <h3>Accede</h3>
-              </Button>
+              <div className="d-grid gap-2">
+                <Button variant="light" size="lg">
+                  <h3>Accede</h3>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
           <Card
@@ -236,9 +243,11 @@ export const Home = () => {
                 Obten las notificaciones oportunas de seguimiento del
                 tratamiento.
               </Card.Text>
-              <Button variant="light" size="lg">
-                <h3>Accede</h3>
-              </Button>
+              <div className="d-grid gap-2">
+                <Button variant="light" size="lg">
+                  <h3>Accede</h3>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
         </Row>
@@ -262,13 +271,14 @@ export const Home = () => {
                 <h2 style={{ textAlign: "center" }}>Utilidades</h2>
               </Card.Title>
               <Card.Text style={{ textAlign: "center" }}>
-                Aprenda acerca de las causas, los síntomas, fases del mal de
-                Alzheimer asi como los avances que se han obtenido
-                recientemente.
+                Aprenda acerca de las causas, los síntomas y fases del Alzheimer
+                asi como los avances que se han obtenido.
               </Card.Text>
-              <Button variant="light" size="lg">
-                <h3>Accede</h3>
-              </Button>
+              <div className="d-grid gap-2">
+                <Button variant="light" size="lg">
+                  <h3>Accede</h3>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
 
@@ -289,12 +299,14 @@ export const Home = () => {
                 <h2 style={{ textAlign: "center" }}>Soy Cuidador</h2>
               </Card.Title>
               <Card.Text style={{ textAlign: "center" }}>
-                Conozca como auto gesrtionarse para afrontar el rol de cuidador
-                y cuidar su salud integral.
+                Conozca como auto gestionarse para afrontar el rol de cuidador y
+                cuidar su salud integral.
               </Card.Text>
-              <Button variant="light" size="lg">
-                <h3>Accede</h3>
-              </Button>
+              <div className="d-grid gap-2">
+                <Button variant="light" size="lg">
+                  <h3>Accede</h3>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
           <Card
@@ -317,9 +329,11 @@ export const Home = () => {
                 Interactua con un Health Coach y recibe un acompañamiento online
                 en tu proceso de salud geográfica.
               </Card.Text>
-              <Button variant="light" size="lg">
-                <h3>Accede</h3>
-              </Button>
+              <div className="d-grid gap-2">
+                <Button variant="light" size="lg">
+                  <h3>Accede</h3>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
           <Card
@@ -339,12 +353,14 @@ export const Home = () => {
                 <h2 style={{ textAlign: "center" }}>Relaciones</h2>
               </Card.Title>
               <Card.Text style={{ textAlign: "center" }}>
-                Revisa tus las notificaciones oportunas de seguimiento del
-                tratamiento.
+                Revisa tus las notificaciones a tiempo para un correcto
+                seguimiento del tratamiento del paciente.
               </Card.Text>
-              <Button variant="light" size="lg">
-                <h3>Accede</h3>
-              </Button>
+              <div className="d-grid gap-2">
+                <Button variant="light" size="lg">
+                  <h3>Accede</h3>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
         </Row>
