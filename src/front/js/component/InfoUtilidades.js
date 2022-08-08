31e77bd5b3facd11ab8b64c8f7f19735cda55064
Robiianna <robidelaparra1@gmail.com>
2022-08-08 16:40:55 +0000

    se agregó las 4 subsecciones a utilidades

--- src/front/js/component/InfoUtilidades.js
+++ src/front/js/component/InfoUtilidades.js
@@ -1,10 +1,140 @@
 import React from "react";
 const InfoUtilidades = () => {
-  return (
-    <>
-      <h1> Aquí van los detalles de las utilidades</h1>
-    </>
-  );
+    return (
+        <>
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div
+                  className="accordion accordion-flush"
+                  id="accordionFlushExample"
+                >
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingOne">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapseOne"
+                        aria-expanded="false"
+                        aria-controls="flush-collapseOne"
+                      >
+                        CONOZCA MÁS
+                      </button>
+                    </h2>
+                    <div
+                      id="flush-collapseOne"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="flush-headingOne"
+                      data-bs-parent="#accordionFlushExample"
+                    >
+                      <div className="accordion-body">
+                        Placeholder content for this accordion, which is intended to
+                        demonstrate the <code>.accordion-flush</code> class. This is
+                        the first item's accordion body.
+                      </div>
+                    </div>
+                  </div>
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingTwo">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapseTwo"
+                        aria-expanded="false"
+                        aria-controls="flush-collapseTwo"
+                      >
+                        CUIDADORES
+                      </button>
+                    </h2>
+                    <div
+                      id="flush-collapseTwo"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="flush-headingTwo"
+                      data-bs-parent="#accordionFlushExample"
+                    >
+                      <div className="accordion-body">
+                        Placeholder content for this accordion, which is intended to
+                        demonstrate the <code>.accordion-flush</code> class. This is
+                        the second item's accordion body. Let's imagine this being
+                        filled with some actual content.
+                      </div>
+                    </div>
+                  </div>
+    
+    
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingThree">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapseThree"
+                        aria-expanded="false"
+                        aria-controls="flush-collapseThree"
+                      >
+                        RECURSOS EN LA RED
+                      </button>
+                    </h2>
+                    <div
+                      id="flush-collapseThree"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="flush-headingThree"
+                      data-bs-parent="#accordionFlushExample"
+                    >
+                      <div className="accordion-body">
+                        Placeholder content for this accordion, which is intended to
+                        demonstrate the <code>.accordion-flush</code> class. This is
+                        the third item's accordion body. Nothing more exciting
+                        happening here in terms of content, but just filling up the
+                        space to make it look, at least at first glance, a bit more
+                        representative of how this would look in a real-world
+                        application.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+    
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-headingFour">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapseFour"
+                        aria-expanded="false"
+                        aria-controls="flush-collapseFour"
+                      >
+                        RUTINAS DE EJERCICIOS
+                      </button>
+                    </h2>
+                    <div
+                      id="flush-collapseFour"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="flush-headingFour"
+                      data-bs-parent="#accordionFlushExample"
+                    >
+                      <div className="accordion-body">
+                        Placeholder content for this accordion, which is intended to
+                        demonstrate the <code>.accordion-flush</code> class. This is
+                        the third item's accordion body. Nothing more exciting
+                        happening here in terms of content, but just filling up the
+                        space to make it look, at least at first glance, a bit more
+                        representative of how this would look in a real-world
+                        application.
+                      </div>
+                    </div>
+                  </div>
+            
+    
+    
+    
+              </div>
+            </div>
+          </div>
+        </>
+      );
 };
 
 export default InfoUtilidades;
