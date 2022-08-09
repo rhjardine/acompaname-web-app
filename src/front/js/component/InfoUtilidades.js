0a644cbf50ae3b8e9ad96029b1d4d14278243452
Robiianna <robidelaparra1@gmail.com>
2022-08-09 16:51:09 +0000

    arreglada completa la sección de utilidades

--- src/front/js/component/InfoUtilidades.js
+++ src/front/js/component/InfoUtilidades.js
@@ -19,7 +19,7 @@ const InfoUtilidades = () => {
                     aria-expanded="false"
                     aria-controls="flush-collapseOne"
                   >
-                    CONOZCA MÁS
+                    <h4>CONOZCA MÁS</h4>
                   </button>
                 </h2>
                 <div
@@ -29,21 +29,22 @@ const InfoUtilidades = () => {
                   data-bs-parent="#accordionFlushExample"
                 >
                   <div className="accordion-body">
-                    ¿Qué es Alzheimer? La enfermedad de Alzheimer es un
+                    <h2> <u>¿Qué es Alzheimer? </u></h2> La enfermedad de Alzheimer es un
                     trastorno cerebral que destruye lentamente la memoria, la
                     capacidad de pensar y, con el tiempo, la habilidad de llevar
                     a cabo hasta las tareas más sencillas.
                     <br></br>
                     <br></br>
-                    Etapas
+                    <h2> <u>Etapas</u></h2>
                     <br></br>
-                    Etapa temprana: la persona puede desenvolverse de forma
+                    <h3>Etapa temprana:</h3> La persona puede desenvolverse de forma
                     independiente. Es posible que pueda conducir, trabajar y
                     participar de actividades sociales. Pero, puede sentir que
                     tiene episodios de pérdida de memoria como olvidarse
                     palabras familiares o la ubicación de objetos cotidianos.
                     <br></br>
-                    Etapa media: es la fase más prolongada, puede durar muchos
+                    <br></br>
+                    <h3>Etapa media:</h3> Es la fase más prolongada, puede durar muchos
                     años. Además de los síntomas anteriores puede presentar
                     desorientación, confundir palabras, cambios en su
                     temperamento como enojo, frustración y actúa de forma
@@ -51,7 +52,8 @@ const InfoUtilidades = () => {
                     cotidianas debido al daño de las células nerviosas del
                     cerebro.
                     <br></br>
-                    Etapa final: las personas pierden la capacidad de responder
+                    <br></br>
+                    <h3>Etapa final:</h3> Las personas pierden la capacidad de responder
                     a su entorno, de llevar adelante una conversación y de
                     controlar los movimientos. A medida que la memoria y las
                     habilidades cognitivas empeoran, es posible que ocurran
@@ -59,23 +61,27 @@ const InfoUtilidades = () => {
                     requieren de ayuda en su totalidad
                     <br></br>
                     <br></br>
-                    ¿Cómo cuidarme?
+                    <h2> <u>¿Cómo cuidarme?</u></h2>
                     <br></br>
                     Con ayuda de su familiar o cuidador deben hacer diversos
                     planes que le permita mantener estimulada la parte
                     cognitiva. Estas son algunas cosas que usted puede hacer:
                     <br></br>
-                    +Haga actividades que concuerde con algo que, siendo
+                    <br></br>
+                    <ul>
+                    <li>Haga actividades que concuerde con algo que, siendo
                     paciente con Alzheimer, puede hacer, ejemplo: tareas
                     domésticas, jugar memoria, pintar, ordenar, visitar
-                    familiares y recordar momentos con fotografías.
+                    familiares y recordar momentos con fotografías.</li>
                     <br></br>
-                    +Seleccione actividades que puedan ser divertidas tanto para
-                    usted como paciente y su familiar o cuidador.
+                    <li>Seleccione actividades que puedan ser divertidas tanto para
+                    usted como paciente y su familiar o cuidador.</li>
                     <br></br>
-                    +Dejarse siempre ayudar.
+                    <li>Dejarse siempre ayudar.</li>
                     <br></br>
-                    +Evite frustrarse, téngase paciencia.
+                    <li>Evite frustrarse, téngase paciencia.</li>
+                    </ul>
+                    
                   </div>
                 </div>
               </div>
@@ -89,7 +95,7 @@ const InfoUtilidades = () => {
                     aria-expanded="false"
                     aria-controls="flush-collapseTwo"
                   >
-                    CUIDADORES
+                    <h4>CUIDADORES</h4>
                   </button>
                 </h2>
                 <div
@@ -99,7 +105,7 @@ const InfoUtilidades = () => {
                   data-bs-parent="#accordionFlushExample"
                 >
                   <div className="accordion-body">
-                    Pasos tras el diagnóstico
+                  <h2> <u>Pasos tras el diagnóstico</u></h2>
                     <br></br>
                     Un ser querido con alzheimer necesitará apoyo en los
                     diferentes ámbitos de su vida tras el diagnóstico, por lo
@@ -110,87 +116,93 @@ const InfoUtilidades = () => {
                     <br></br>
                     Después de ello tome en cuenta lo siguiente:
                     <br></br>
-                    <br></br>• Asumir nuevas responsabilidades.
-                    <br></br>• Prevenir y siempre estar atento a posibles retos
-                    y problemas.
                     <br></br>
-                    •Promueva actividades que sean estimulantes y que pueda
-                    disfrutar su familiar con alzhéimer.
+                    <ul>
+                      <li>Asumir nuevas responsabilidades.</li>
                     <br></br>
-                    •Tener personas y objetos comunes alrededor. Puede ser útil
-                    tener álbumes de fotos familiares.
+                    <li>Prevenir y siempre estar atento a posibles retos
+                    y problemas.</li>
                     <br></br>
-                    •Brindar recordatorios, notas, listas de tareas rutinarias o
-                    instrucciones para las actividades diarias.
+                    <li>Promueva actividades que sean estimulantes y que pueda
+                    disfrutar su familiar con alzhéimer.</li>
                     <br></br>
-                    •Ceñirse a un horario de actividades simple.
+                    <li>Tener personas y objetos comunes alrededor. Puede ser útil
+                    tener álbumes de fotos familiares.</li>
                     <br></br>
-                    •Hablar con su familiar de cualquier tema que sean eventos
-                    actuales.
+                    <li>Brindar recordatorios, notas, listas de tareas rutinarias o
+                    instrucciones para las actividades diarias.</li>
                     <br></br>
-                    •Tener siempre los números de emergencia guardados.
+                    <li>Ceñirse a un horario de actividades simple.</li>
                     <br></br>
-                    •Incrementar la seguridad en la casa, para evitar
-                    accidentes.
+                    <li>Hablar con su familiar de cualquier tema que sean eventos
+                    actuales.</li>
                     <br></br>
-                    •Apoyo médico, de otros familiares, conocidos o vecinos.
+                    <li>Tener siempre los números de emergencia guardados.</li>
+                    <br></br>
+                    <li>Incrementar la seguridad en la casa, para evitar
+                    accidentes.</li>
+                    <br></br>
+                    <li>Apoyo médico, de otros familiares, conocidos o vecinos.
                     También apoyarse de ONG’s e instituciones o cuidadores
-                    especializados en la materia.
+                    especializados en la materia.</li></ul> 
+                  
                     <br></br>
+                    <h2> <u>Actuar en cada Etapa</u></h2>
                     <br></br>
-                    Actuar en cada Etapa
-                    <br></br>
-                    <br></br>
-                    Etapa temprana:
+                    <h3>Etapa temprana:</h3>
                     <br></br>
                     El objetivo es que mantenga el bienestar físico, la salud y
                     tranquilidad.
                     <br></br>
-                    •Potenciar su independencia: que haga por sí mismo cuanto le
+                    <br></br>
+                    <ul>
+                      <li>Potenciar su independencia: que haga por sí mismo cuanto le
                     sea posible para que así no olvide sus rutinas, se mantenga
-                    útil, autónomo y no decaiga su autoestima.
+                    útil, autónomo y no decaiga su autoestima.</li>
                     <br></br>
-                    •Ayudar al paciente a planificar: sea actividades que puede
-                    ayudar a mantener activa la parte cognitiva.
+                    <li>Ayudar al paciente a planificar: sea actividades que puede
+                    ayudar a mantener activa la parte cognitiva.</li>
                     <br></br>
-                    •Prevenir cualquier accidente.
+                    <li>Prevenir cualquier accidente.</li></ul>
                     <br></br>
-                    <br></br>
-                    Etapa media:
+                    <h3>Etapa media:</h3>
                     <br></br>
                     Esta fase supone una pérdida progresiva de su autonomía, la
                     familia debe informarse de lugares que brinden apoyo.
                     <br></br>
-                    •Formar equipos familiares: que puedan ayudar al paciente
+                    <br></br>
+                    <ul>
+                      <li>Formar equipos familiares: que puedan ayudar al paciente
                     porque y se turnen en sus cuidados, pues para una persona
-                    agota física y psicológicamente.
+                    agota física y psicológicamente.</li>
                     <br></br>
-                    •Buscar centros de ayuda o cuidadores: que les permitan
-                    cuidar al paciente por algunas horas.
+                    <li>Buscar centros de ayuda o cuidadores: que les permitan
+                    cuidar al paciente por algunas horas.</li>
                     <br></br>
-                    •Centros de estimulación cognitiva: buscar lugares donde el
-                    paciente pueda hacer ejercicio físico y mental.
+                    <li>Centros de estimulación cognitiva: buscar lugares donde el
+                    paciente pueda hacer ejercicio físico y mental.</li></ul>
                     <br></br>
-                    <br></br>
-                    Etapa final:
+                    <h3>Etapa final:</h3>
                     <br></br>
                     Es la fase más complicada y a su vez más retadora tanto para
                     el paciente como para el familiar, por lo que ambas partes
                     requieren apoyo de especialistas.
                     <br></br>
-                    •Apoyarse en familia: se deben turnar con los cuidados del
+                    <br></br>
+                    <ul>
+                      <li>Apoyarse en familia: se deben turnar con los cuidados del
                     paciente, ya que requieren de más atención. También
-                    establecer planes de emergencia.
+                    establecer planes de emergencia.</li>
                     <br></br>
-                    •Buscar apoyo psicológico: para familiares y cuidadores
+                    <li>Buscar apoyo psicológico: para familiares y cuidadores
                     porque la enfermedad desgasta mentalmente y puede presentar
-                    síndrome del cuidador principal.
+                    síndrome del cuidador principal.</li>
                     <br></br>
-                    •Buscar apoyo médico: no solo para el paciente sino también
-                    para familiares porque agota físicamente para ellos.
+                    <li>Buscar apoyo médico: no solo para el paciente sino también
+                    para familiares porque agota físicamente para ellos.</li></ul>
+                    
                     <br></br>
-                    <br></br>
-                    Síndrome del cuidador
+                    <h2> <u>Síndrome del cuidador</u></h2>
                     <br></br>
                     El síndrome del cuidador es un conjunto de alteraciones
                     físicas, psíquicas y sociales que suele experimentar el
@@ -219,7 +231,7 @@ const InfoUtilidades = () => {
                     aria-expanded="false"
                     aria-controls="flush-collapseThree"
                   >
-                    RECURSOS EN LA RED
+                    <h4>RECURSOS EN LA RED</h4>
                   </button>
                 </h2>
                 <div
@@ -229,31 +241,31 @@ const InfoUtilidades = () => {
                   data-bs-parent="#accordionFlushExample"
                 >
                   <div className="accordion-body">
-                  Mira las últimas publicaciones sobre alzheimer:
+                  <h2> <u>Mira las últimas publicaciones sobre alzheimer:</u></h2>
                   <br></br>
-                  Identificadas variantes en APOE que reducen riesgo de desarrollar Alzheimer (haz clic aquí)
+                  <h6>Identificadas variantes en APOE que reducen riesgo de desarrollar Alzheimer <a href="https://genotipia.com/genetica_medica_news/apoe4-alzheimer-2/">(Haga clic aquí)</a></h6>
                   <br></br>
-                  Mejora el mapa genómico de la enfermedad de Alzheimer (haz clic aquí)
+                  <h6>Mejora el mapa genómico de la enfermedad de Alzheimer <a href="https://genotipia.com/genetica_medica_news/mejora-el-mapa-genomico-alzheimer/">(Haga clic aquí)</a></h6>
                   <br></br>
-                  Descubren una proteína que protege al cerebro de la enfermedad de Alzheimer (haz clic aquí)
-                  <br></br>
-                  <br></br>
-                  Manuales sobre alzhéimer:
-                  <br></br>
-                  Guía para Familiares de Enfermos de Alzheimer:“Querer cuidar, saber hacerlo” (haz clic aquí)
-                  <br></br>
-                  Manual para el cuidador de pacientes con demencia (haz clic aquí)
-                  <br></br>
-                  La Enfermedad de Alzheimer: Manual del cuidador (haz clic aquí)
+                  <h6>Descubren una proteína que protege al cerebro de la enfermedad de Alzheimer <a href="https://genotipia.com/genetica_medica_news/proteina-protege-alzheimer/">(Haga clic aquí)</a></h6>
                   <br></br>
                   <br></br>
-                  Películas, documentales y series:
+                  <h2> <u>Manuales sobre alzhéimer:</u></h2>
                   <br></br>
-                  Piedad. Documental sobre el alzhéimer. Por Otto Roca.
+                  <h6>Guía para Familiares de Enfermos de Alzheimer:“Querer cuidar, saber hacerlo” <a href="https://fiapam.org/wp-content/uploads/2013/12/GuiaAlzheimer.pdf">(Haga clic aquí)</a></h6>
                   <br></br>
-                  Still Alice. Película sobre la vida de una profesora tras ser diagnosticada con alzheimer. Por Richard Glatzer, Wash Westmoreland.
+                  <h6>Manual para el cuidador de pacientes con demencia <a href="https://www.imserso.es/InterPresent1/groups/imserso/documents/binario/03_manual_pacientesdemencia.pdf">(Hagaclic aquí)</a></h6>
                   <br></br>
-                  Bicicleta, cuchara y manzana. Documental sobre la vida del político catalán, Pasqual Maragall, y su enfermedad alzhéimer. Por Carles Bosch.
+                  <h6>La Enfermedad de Alzheimer: Manual del cuidador <a href="https://www.snpv.org/contenidos/files/snpv_enlaces/0003.pdf">(Haga clic aquí)</a></h6>
+                  <br></br>
+                  <br></br>
+                  <h2> <u>Películas, documentales y series:</u></h2>
+                  <br></br>
+                  <h6>Piedad. Documental sobre el alzhéimer. Por Otto Roca.</h6>
+                  <br></br>
+                  <h6>Still Alice. Película sobre la vida de una profesora tras ser diagnosticada con alzheimer. Por Richard Glatzer, Wash Westmoreland.</h6>
+                  <br></br>
+                  <h6>Bicicleta, cuchara y manzana. Documental sobre la vida del político catalán, Pasqual Maragall, y su enfermedad alzhéimer. Por Carles Bosch.</h6>
 
                   </div>
                 </div>
@@ -270,7 +282,7 @@ const InfoUtilidades = () => {
                   aria-expanded="false"
                   aria-controls="flush-collapseFour"
                 >
-                  RUTINAS DE EJERCICIOS
+                  <h4>RUTINAS DE EJERCICIOS</h4>
                 </button>
               </h2>
               <div
@@ -281,11 +293,11 @@ const InfoUtilidades = () => {
               >
                 <div className="accordion-body">
 
-                Ejercicios para personas con alzheimer en etapa inicial (haz clic aquí)
+                <h6>Ejercicios para personas con alzheimer en etapa inicial <a href="https://www.youtube.com/watch?v=VBZCRtfCOoE">(Haga clic aquí)</a></h6>
                 <br></br>
-                Ejercicios para mejorar el equilibrio y marcha autónoma (haz clic aquí)
+                <h6>Ejercicios para mejorar el equilibrio y marcha autónoma <a href="https://www.youtube.com/watch?v=Ta3O86qZrms">(Haga clic aquí)</a></h6>
                 <br></br>
-                Ejercicio de fisioterapia para personas con alzheimer (haz clic aquí)
+                <h6>Ejercicio de fisioterapia para personas con alzheimer <a href="https://www.youtube.com/watch?v=tewMao6oWik">(Haga clic aquí)</a></h6>
 
                 </div>
               </div>
