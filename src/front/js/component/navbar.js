be65e2a34e28c3763055b8df1b1c5320ee273f13
Azim3triko <azim3triko@protonmail.com>
2022-08-10 00:41:42 +0000

    se agrega logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../pages/Imagenes/Logo_A.png";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{ backgroundColor: "#36626a" }}
+    >
+      <div className="navbar-brand" href="#">
+        <img
+          className="imagen"
+          width="100px"
+          height="100px"
+          style={{ borderRadius: "50px" }}
+          src={logo}
+        ></img>
+      </div>
+      {/* <button
         className="navbar-toggler"
         type="button"
         data-toggle="collapse"
@@ -43,7 +53,7 @@ export const Navbar = () => {
             </Link>
           </li>
         </ul>
-      </div>
+      </div> */}
     </nav>
   );
 };
